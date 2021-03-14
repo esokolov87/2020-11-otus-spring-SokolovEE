65e277aca5a44112f0c04cbec94296f8f31a55dd
eldar <sokolov_eldar@mail.ru>
2021-03-14 22:39:07 +0300

    ДЗ: В CRUD Web-приложение добавить механизм аутентификации

---
+++ spring-12/src/main/java/ru/sokolovee/spring12/controllers/rest/BookRestController.java
@@ -0,0 +1,31 @@
+package ru.sokolovee.spring12.controllers.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.sokolovee.spring12.entities.Book;
+import ru.sokolovee.spring12.repositories.AuthorRepository;
+import ru.sokolovee.spring12.repositories.BookRepository;
+import ru.sokolovee.spring12.repositories.GenreRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class BookRestController {
+    private final AuthorRepository authorRepository;
+    private final GenreRepository genreRepository;
+    private final BookRepository bookRepository;
+
+    @PatchMapping("/book/{id}")
+    public void updateBook(@PathVariable Long id, @RequestParam String name, @RequestParam Long authorid, @RequestParam Long genreid) {
+        Book book = bookRepository.getOne(id);
+        book.setName(name);
+        book.setAuthor(authorRepository.getOne(authorid));
+        book.setGenre(genreRepository.getOne(genreid));
+        bookRepository.save(book);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+
+}
---
+++ spring-12/src/test/java/ru/sokolovee/spring12/Spring12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sokolovee.spring12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
