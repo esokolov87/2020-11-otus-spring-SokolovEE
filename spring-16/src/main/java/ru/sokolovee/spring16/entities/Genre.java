570bb3f1abb83368edcfc490ad9e3b8da9adbce2
eldar <sokolov_eldar@mail.ru>
2021-03-29 20:05:13 +0300

    ДЗ: Использовать метрики, healthchecks и logfile

---
+++ spring-16/src/main/java/ru/sokolovee/spring16/entities/Genre.java
@@ -0,0 +1,25 @@
+package ru.sokolovee.spring16.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Genre")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GENERATOR_GENRE_SEQ")
+    @SequenceGenerator(name = "GENERATOR_GENRE_SEQ", sequenceName = "GENRE_SEQ", initialValue = 5, allocationSize = 1)
+    private Long id;
+    private String name;
+
+    public Genre(Long id) {
+        this.id = id;
+    }
+}
---
+++ spring-16/src/main/resources/data.sql
@@ -0,0 +1,37 @@
+insert into genre (id, name)
+values (1, 'Фантастика'),
+       (2, 'Научная литература'),
+       (3, 'Ужасы'),
+       (4, 'Классика');
+
+insert into author (id, name)
+values (1, 'Пушкин А.С.'),
+       (2, 'Стивен Кинг'),
+       (3, 'Джорж Мартин'),
+       (4, 'Стивен Хокинг');
+
+insert into comment (id, book_id, comment)
+values (1, 1, 'Книга Пушкина А.С. "Руслан и Людмила", жанр Классика'),
+       (2, 3, 'Стивен Хокинга "Краткая история времени", жанр Научная литература'),
+       (3, 4, 'Книга Стивен Кинга "Оно", жанр Ужасы'),
+       (4, 2, 'Джорж Мартина "Игра престолов", жанр Фантастика');
+
+insert into book (id, name, author_id, genre_id)
+values (1, 'Руслан и Людмила', 1, 4),
+       (2, 'Игра престолов', 3, 1),
+       (3, 'Краткая история времени', 4, 2),
+       (4, 'Оно', 2, 3);
+
+
+CREATE SEQUENCE BOOK_SEQ START WITH 5 INCREMENT BY 1;
+CREATE SEQUENCE AUTHOR_SEQ START WITH 5 INCREMENT BY 1;
+CREATE SEQUENCE GENRE_SEQ START WITH 5 INCREMENT BY 1;
+CREATE SEQUENCE COMMENT_SEQ START WITH 5 INCREMENT BY 1;
+
+INSERT INTO users (username, password, enabled)
+values  ('user', 'user', 1),
+        ('admin', 'admin', 1);
+
+INSERT INTO authorities (username, authority)
+values  ('user', 'ROLE_USER'),
+        ('admin', 'ROLE_ADMIN');
