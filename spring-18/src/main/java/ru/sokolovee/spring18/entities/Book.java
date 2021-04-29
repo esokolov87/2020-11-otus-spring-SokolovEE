abb5d431dc35857a27f872123a3dca8093796704
eldar <sokolov_eldar@mail.ru>
2021-04-29 19:43:38 +0300

    ДЗ: Обернуть внешние вызовы в Hystrix

---
+++ spring-18/src/main/java/ru/sokolovee/spring18/entities/Book.java
@@ -0,0 +1,57 @@
+package ru.sokolovee.spring18.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Book")
+@NamedEntityGraph(name = "book-entity-graph", attributeNodes = {@NamedAttributeNode("author"), @NamedAttributeNode("genre")})
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GENERATOR_BOOK_SEQ")
+    @SequenceGenerator(name = "GENERATOR_BOOK_SEQ", sequenceName = "BOOK_SEQ", initialValue = 5, allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    @Fetch(FetchMode.JOIN)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @Fetch(FetchMode.JOIN)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genre genre;
+
+    public Book(String name, Author author, Genre genre) {
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public Book(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author=" + author +
+                ", genre=" + genre +
+                '}';
+    }
+}
