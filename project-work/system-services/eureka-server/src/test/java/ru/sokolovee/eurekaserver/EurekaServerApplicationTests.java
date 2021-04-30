68be2120f35ef67d6ab0f94ea82d60b71b18caf4
eldar <sokolov_eldar@mail.ru>
2021-04-30 17:31:21 +0300

    init commit

---
+++ project-work/system-services/eureka-server/src/test/java/ru/sokolovee/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.ru.sokolovee.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
